360ecdcac1206dca820df2014039b9a7b350a556
juannpg <juan.pasgut.workspace@gmail.com>
2024-08-04 18:53:10 +0200

    auto push
    commit automatizado

--- frontend/src/dashboard/Dashboard.tsx
+++ frontend/src/dashboard/Dashboard.tsx
@@ -2,6 +2,14 @@ import '../globals.css'
 import './Dashboard.css'
 
 function Dashboard() {
+  function tweetFeed() {
+    return (
+      <div className='tweetFeed'>
+        <p>Hola mundo</p>
+        
+      </div>
+    )
+  }
 
   return (
     <div className="main">
@@ -11,6 +19,13 @@ function Dashboard() {
       <div className="body">
         <div className="dashboard">
           <h2>Bienvenido a X, <span className='username'>{localStorage.getItem('username')}</span>.</h2>
+          <div className="tweet">
+            <h3>Escribir un tweet</h3>
+          </div>
+          <div className="feed">
+            <h3>Tweets recientes</h3>
+              {tweetFeed()}
+          </div>
         </div>
       </div>
     </div>
